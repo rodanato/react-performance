3cfd129ba7eee2178209e0ae20d8030ec7f9f781
rod <rod.a.nato@gmail.com>
2018-10-24 17:34:00 -0500

    fix(): wrong api url

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,6 +1,7 @@
 // @flow
 
 // const API = 'https://ema-backend-dev.appspot.com/_ah/api/nina/';
-const API = 'http://localhost:8080/api/';
+// const API = 'http://localhost:8080/api/';
+const API = 'https://social-music-api-rjnwpizwca.now.sh/api/';
 
 export const RESTResolve = (restEndpoint: string) => API + restEndpoint;
